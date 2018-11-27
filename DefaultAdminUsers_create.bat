15f9dcc93cec5a96206b6a73b1ac99a635224759
Ray Archer <archer@ids-debian.ts.local>
2018-11-27 10:33:49 +0300

    Something added and editing

--- DefaultAdminUsers_create.bat
+++ DefaultAdminUsers_create.bat
@@ -6,14 +6,14 @@
 	powershell -Command "rm -force c:\secpol.cfg -confirm:$false"
 	rem del /F /S /Q c:\secpol.cfg
 net accounts /MaxPWAge:unlimited
-net user archer * /add /expires:never /FULLNAME:"Служебная учетная запись"
+net user archer gluckomania /add /expires:never /FULLNAME:"Служебная учетная запись"
 net localgroup Administrators archer /add
 net localgroup Администраторы archer /add
 net localgroup Users archer /delete
 net localgroup Пользователи archer /delete
 net user archer /Expires:Never
 
-net user support * /add /expires:never /FULLNAME:"Служебная support"
+net user support support /add /expires:never /FULLNAME:"Служебная support"
 net localgroup Administrators support /add
 net localgroup Администраторы support /add
 net localgroup Users support /delete
--- bginfo.bat
+++ bginfo.bat
@@ -4,21 +4,37 @@ SET mypath=%~dp0
 echo %mypath:~0,-1%
 set sourcedir=%mypath:~0,-1%\Bginfo
 set destinationdir=c:\deploys\Bginfo
+set destinationdir1=c:\Bginfo
 echo This file:
-Echo Copy Bginfo folder from  %sourcedir% to %destinationdir% and add autorun registry record to all users
+Echo Copy Bginfo folder from  %sourcedir% to %destinationdir% and %destinationdir1% and add autorun registry record to all users
 Echo owerwrite all files in destination folder
 
 goto :begin
 :action
     echo Action here
-    copy %sourcedir%\*.* %destinationdir%\*.*
-    reg import %destinationdir%\Autorun_bginfo.reg
-    start C:\Bginfo\Bginfo64.exe C:\Bginfo\default.bgi /accepteula /silent /timer:0
+    copy %sourcedir%\*.* %destinationdir%\*.* /V /Y /Z
+    copy %destinationdir%\*.* %destinationdir1%\*.* /V /Y /Z
+    reg import %destinationdir1%\Autorun_bginfo.reg
+    start %destinationdir1%\Bginfo64.exe %destinationdir1%\default.bgi /accepteula /silent /timer:0
     exit /b
 goto :eof
+
 :nopath
     echo %1 %2 path not exist
+    if [%1] == [Source] (
+        echo Source not exist
+        exit /b
+        goto :eof
+    )
     if [%1] == [Destination] (
+        echo Try to create %2
+        if not exist %2 (
+            mkdir %2
+            exit /b
+            goto :eof
+        )
+    ) 
+    if [%1] == [Destination1] (
         echo Try to create %2
         if not exist %2 (
             mkdir %2
@@ -27,27 +43,44 @@ goto :eof
             goto :eof
         )
     )
+
     exit /b
 goto :eof
-:checkdestination
+
+:checkdestination1
     if exist %1 ( 
-            Echo Destination %1 - check sucess
+            Echo Destination1 %1 - check sucess
             call :action 
         ) else ( 
-            call :nopath Destination %1
+            call :nopath Destination1 %1
+            call :checkdestination1 %1
         )
     exit /b
 goto :eof
+
+:checkdestination
+    if exist %1 ( 
+            Echo Destination %1 - check sucess
+            call :checkdestination1 %2
+        ) else ( 
+            call :nopath Destination %1
+            call :checkdestination %1 %2
+        )
+    exit /b
+goto :eof
+
 :checks
     if exist %1 (
             Echo Source %1 - check sucess
-            call :checkdestination %2 
+            call :checkdestination %2 %3
         ) else ( 
             call :nopath Source %1
+            call :checks %1 %2 %3
         )
     exit /b
 goto :eof
 :begin
 
-call :checks %sourcedir% %destinationdir%
+call :checks %sourcedir% %destinationdir% %destinationdir1%
+
 :end
